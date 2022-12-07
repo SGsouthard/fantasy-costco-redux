d7edb1e56979de61d4751e38a933de617fdcb14b
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2022-12-07 06:31:48 -0800

    more build.sh updates

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 
 pip3 install --upgrade pip
-pip3 install django
-pip3 install dj-database-url
+pip3 install django dj-database-url psycopg2-binary
+# pip3 install 
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
