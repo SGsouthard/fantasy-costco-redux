bb88a3cbe035a5e57f6043f7c56b9c43c88c6491
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2022-12-07 05:51:33 -0800

    updated settings.py?

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -139,7 +139,7 @@ if not DEBUG:
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
