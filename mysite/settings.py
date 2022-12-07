08a1b064ee51626549d565a980bb014c6ed334d8
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2022-12-07 06:21:43 -0800

    moved import database url to top of file?

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import dj_database_url
 # import django_heroku
 from pathlib import Path
 import os
 from os import environ
 
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
