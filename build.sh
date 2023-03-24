807711f1570ba56e0fd3974b9f638ce00c72caf7
Sydney GSouthard <92966604+SGsouthard@users.noreply.github.com>
2023-03-24 01:18:11 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 pip3 install --upgrade pip
 pip3 install django dj-database-url psycopg2-binary 'whitenoise[brotli]' gunicorn
-pip3 install -r requirements.txt
+# pip3 install -r requirements.txt
 # pip3 install 
 
 python3 manage.py collectstatic --no-input
